d9246e689d424dbe6c1fa251e453e84c5bd903a7
axbdc <alexandrexbdcosme@gmail.com>
2026-05-21 14:40:37 +0100

    restore tailwindcss vite plugin

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,6 @@
 import path from "path";
 import { fileURLToPath } from "url";
+import tailwindcss from "@tailwindcss/vite";
 import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
 
@@ -7,7 +8,7 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), tailwindcss()],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "src"),
